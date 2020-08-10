a94c4ec43b66d57093b9e1d0efd92315e1ed0f2b
vagrant <vagrant@homestead>
2020-08-10 12:37:30 +0000

    Cambios para proxy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 use Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,6 +18,19 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Carbon::setLocale(config('app.locale'));
+
+        $proxy_url    = getenv('PROXY_URL');
+        $proxy_schema = getenv('PROXY_SCHEMA');
+        
+        if(!empty($proxy_url)) {
+            URL::forceRootUrl($proxy_url);
+        }
+
+        if(!empty($proxy_schema)) {
+            URL::forceScheme($proxy_schema);
+        }
+
+        
     }
 
     /**
